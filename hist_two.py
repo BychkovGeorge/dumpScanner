23b820859f81391aa66c199892bbe332da7fb662
g.bychkov <g.bychkov@grcc.tech>
2021-03-28 12:35:53 +0300

    first commit

---
+++ hist_two.py
@@ -0,0 +1,42 @@
+import csv
+import matplotlib.pyplot as plt
+
+
+def file_reader(file_name):
+    with open(file_name, encoding='utf-8') as r_file:
+        dump = csv.reader(r_file, delimiter=",")
+        count = 0
+        for row in dump:
+            if count > 99:
+                times.append(row[1])
+            count += 1
+
+
+times = []
+intervals = []
+file_reader('dump.csv')
+
+for i in range(len(times)):
+    if i != len(times) - 1:
+        intervals.append(float(times[i + 1]) - float(times[i]))
+
+distributed_intervals = []
+counts = []
+
+temp = 0
+while temp < 4.7:
+    distributed_intervals.append(temp)
+    counts.append(0)
+    temp += 0.1
+
+for i in range(len(intervals)):
+    for j in range(len(distributed_intervals)):
+        if distributed_intervals[j] < intervals[i] < distributed_intervals[j + 1]:
+            counts[j] += 1
+            break
+
+fig, ax = plt.subplots()
+ax.bar(distributed_intervals, counts)
+plt.show()
+
+# вероятность присутствия скрытого канала стремится к 100% = (1 - 0 / 12) * 100
